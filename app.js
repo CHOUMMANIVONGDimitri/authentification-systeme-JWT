b8c073cd4b6ab37df88bca4cedfa79c8e3b426eb
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-12-12 14:55:23 +0100

    challenge express 08 authentification with JWT

--- app.js
+++ app.js
@@ -8,27 +8,47 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 3000;
 
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+/* Home page route */
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
+// the public routes
+
+/* movies GET routes */
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+/* users GET routes */
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+/* login route */
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+/* create users */
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+// the private routes
+
+app.use(verifyToken); // <= the authentification wall
+
+/* movies POST, PUT, DELETE routes */
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
+/* users PUT, DELETE routes */
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
